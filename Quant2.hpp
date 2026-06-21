d9f6339f44258825143cbd280f1b743f4c994547
Kudasi <dan0406qwerty@gmail.com>
2026-06-21 13:16:02 +0300

    Hotfix: PORTC -> PINC in Quant2

--- Quant2.hpp
+++ Quant2.hpp
@@ -24,7 +24,7 @@ public:
   }
 
   void readLineSensors() override {
-    lineSensors = ~PORTC & 31;
+    lineSensors = ~PINC & 31;
     
     if constexpr (Debug) {
       Serial.print("Line sensors updated: [ ");
