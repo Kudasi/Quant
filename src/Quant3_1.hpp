b366cda1e78bfb05c3aae2960d3bc35624827cde
Kudasi <dan0406qwerty@gmail.com>
2026-06-30 16:03:25 +0300

    Refactor WS_ANALOG_TO_RELATIVE macro and update wall sensor reading; removed unused IMU initialization code.

--- src/Quant3_1.hpp
+++ src/Quant3_1.hpp
@@ -9,16 +9,8 @@
 #define MUX_IN3 A1
 #define MUX_OUT A0
 
-#define WS_ANALOG_TO_RELATIVE(pin) (pow(analogRead(pin) / 400.0, 0.6))
+#define WS_ANALOG_TO_RELATIVE(pin) (1.0 - pow(analogRead(pin) / 1000.0, 1.0))
 
-#define MPU_ADDR 0x68
-#define MPU_CONFIG 0x1A
-#define MPU_ACCEL_CONFIG 0x1C
-#define MPU_ACCEL_START 0x3B
-#define MPU_POWER_MGMT 0x6B
-
-#define SCALE 16384.0
-#define GRAVITY 9.80665
 
 template <uint16_t Features>
 class Quant<Q3_1, Features> : public QuantBase<6, 3, Features>
@@ -39,35 +31,11 @@ public:
     pinMode(MUX_IN2, OUTPUT);
     pinMode(MUX_IN3, OUTPUT);
 
-    Wire.begin();
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(MPU_POWER_MGMT);
-    Wire.write(0);
-    int err = Wire.endTransmission();
 
     if constexpr (Features & DEBUG)
     {
-      if (err)
-      {
-        Serial.print("Error: IMU initialization failed! Code: ");
-        Serial.println(err);
-      }
       Serial.println("Running version 3.1!");
     }
-
-    if (err)
-      return;
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(MPU_ACCEL_CONFIG);
-    Wire.write(0);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(MPU_CONFIG);
-    Wire.write(0x4);
-    Wire.endTransmission();
   }
 
   void _readLineSensors() override
@@ -80,7 +48,7 @@ public:
       digitalWrite(MUX_IN3, (i / 4) % 2);
 
       this->lineSensorBits <<= 1;
-      this->lineSensorBits |= digitalRead(MUX_OUT);
+      this->lineSensorBits |= !digitalRead(MUX_OUT);
     }
 
     if constexpr (Features & DEBUG) lineSensorsDebug();
@@ -93,7 +61,7 @@ public:
 
   void _readWallSensors() override
   {
-    this->wallSensorValues[0] = WS_ANALOG_TO_RELATIVE(5);
+    this->wallSensorValues[0] = WS_ANALOG_TO_RELATIVE(3);
     this->wallSensorValues[1] = WS_ANALOG_TO_RELATIVE(6);
     this->wallSensorValues[2] = WS_ANALOG_TO_RELATIVE(7);
 
